1d783fbe28ad8a8fe8e9e51cd4c316f0fdc7cdae
Ramazan <rmustakimov@neklo.com>
2023-05-21 14:25:35 +0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+let gulp     = require('gulp'),
+    browserSync  = require('browser-sync').create(),
+    uglify       = require('gulp-uglify'),
+    fileinclude = require('gulp-file-include'),
+    imagemin = require('gulp-imagemin'),
+    less         = require('gulp-less');
+
+gulp.task('html_build', function (done) {
+    return gulp.src('src/*.html')
+        .pipe(fileinclude())
+        .pipe(gulp.dest('build/'))
+        .pipe(browserSync.stream());
+    done();
+});
+
+gulp.task('css_build', function (done) {
+    return gulp.src('src/less/main.less')
+        .pipe(less())
+        .pipe(gulp.dest('build/css/'))
+        .pipe(browserSync.stream());
+    done();
+});
+
+gulp.task('image', function (done) {
+    gulp.src('src/img/**')
+        .pipe(imagemin())
+        .pipe(gulp.dest('build/img'))
+        .pipe(browserSync.stream());
+    done();
+});
+
+gulp.task('js_build', function (done) {
+    return gulp.src('src/js/main.js')
+        .pipe(fileinclude())
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js/'))
+        .pipe(browserSync.stream());
+    done();
+});
+
+gulp.task('webServer', function(done) {
+    browserSync.init({
+        server: "build/"
+    });
+    gulp.watch('src/**/*.html', gulp.series('html_build'));
+    gulp.watch('src/**/*.less', gulp.series('css_build'));
+    gulp.watch('src/**/*.js', gulp.series('js_build'));
+    done();
+});
+
+gulp.task('default', gulp.series('html_build', 'image', 'css_build', 'js_build', 'webServer'));
---
+++ src/js/main.js
@@ -0,0 +1,57 @@
+fetch('https://banners-website.wildberries.ru/public/v1/banners?urltype=1024&apptype=1&displaytype=2&longitude=37.6201&latitude=55.753737&country=1&culture=ru')
+    .then((res) => res.json())
+    .then((d) => {
+        document.querySelectorAll('.swiper-slide__img').forEach((el, i) => {
+            return (el.src = 'https://static-basket-01.wb.ru/vol1/crm-bnrs' + d[i].Src)
+        });
+    });
+
+var swiper = new Swiper('.swiper', {
+    speed: 500,
+    effect: 'fade',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+    debugger: true,
+});
+
+const images = [
+    { src: 'img/banner-1.png' },
+    { src: 'img/banner-2.png' },
+    { src: 'img/banner-3.png' },
+    { src: 'img/banner-4.png' },
+    { src: 'img/banner-5.png' },
+    { src: 'img/banner-6.png' },
+    { src: 'img/banner-7.png' },
+    { src: 'img/banner-8.png' }
+];
+
+var gallery = document.querySelector(".gallery");
+
+var ul = document.createElement("ul");
+ul.className = 'gallery__list';
+for (var i = 0; i < images.length; i++) {
+    var image = document.createElement("img"),
+        li = document.createElement("li"),
+        a = document.createElement("a");
+    image.src = images[i].src;
+    image.alt = "Image " + (i+1);
+    a.href = "#" + (i+1);
+
+    li.className = 'gallery__list--item';
+    image.className = 'gallery__list--img';
+    a.className = 'gallery__list--link';
+
+    li.appendChild(a);
+    a.appendChild(image);
+    ul.appendChild(li);
+}
+
+gallery.appendChild(ul);
